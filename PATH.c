c5e2c2083bb0422d93c434bf401007e347fa235f
zinininabil-stack <zinini.nabil@gmail.com>
2025-12-16 16:15:41 +0100

    feat: Try to create linked list structure for directory paths and initial read_line function

---
+++ PATH.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+
+
+*/
+typedef struct node {
+	char *directory
+	struct node *next;
+}
+
+struct node *path_to_directory
+	struct node *head = NULL;
+	struct node *tail = NULL;
+
+char *read_line(input)  /** Getline, user input */
+
+token = strtok(buffer, " ")
+while (token != NULL)
+	{
+
+			
+	}
+
+
+
+
+
+
