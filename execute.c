4dc57003a6c9bb6ac9c94bbf686359f044b765a7
alexandre-mouysset <12258@holbertonstudents.com>
2025-12-18 10:48:56 +0100

    updated execute_command to accept env parameter and handle /

--- execute.c
+++ execute.c
@@ -7,11 +7,15 @@
  *
  * Return: 0 on success, 1 on failure
  */
-int execute_command(char *line)
+int execute_command(char *line, char **env)
 {
 	char *argv[100];
 	pid_t pid;
 	int argc;
+	char *path;
+	char *path_copy;
+	char *directory;
+	char full_path[1000];
 
 	if (line == NULL)
 		return (1);
@@ -26,9 +30,31 @@ int execute_command(char *line)
 
 	if (pid == 0)
 	{
-		execve(argv[0], argv, NULL);
-		fprintf(stderr, "Error: command does not exist -> %s\n", argv[0]);
-		exit(-1);
+		if (strchr(argv[0], '/') != NULL)
+		{
+			execve(argv[0], argv, env);
+			fprintf(stderr, "Error: command does not exist -> %s\n", argv[0]);
+			exit(1);
+		}
+
+		path = get_path(env);
+		if (path)
+		{
+			path_copy = strdup(path);
+			directory = strtok(path_copy, ":");
+
+			while (directory)
+			{
+				sprintf(full_path, "%s/%s", directory, argv[0]);
+				execve(full_path, argv, env);
+				directory = strtok(NULL, ":");
+			}
+
+			free(path_copy);
+		}
+
+		fprintf(stderr, "Error: command not found -> %s\n", argv[0]);
+		exit(1);
 	}
 
 	wait(NULL);
